7463c49b0eb3ec711bcaf27d5b8ed36677b7cb51
md asfak <skmdashfakurrahman@gmail.com>
2024-04-14 14:52:28 +0530

    Sorry Sir

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Redux } from "./Redux/Redux";
+
+export default function App() {
+  return (
+    <>
+      <center>
+        <h1>Evulation 10</h1>
+        <Redux />
+      </center>
+    </>
+  );
+}
---
+++ src/Redux/Redux.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+export const Redux = () => {
+  return (
+    <div>
+      <h1>Dear Sir</h1>
+      <h2>Hyper Tech Global Techlology </h2>
+      <h3>Up,India</h3>
+      <p>
+        Dear Sir me Ramzan Or Eid Ke Liye Me Kuch Practice Nehi Kar Paya. Isliye
+        Vs Code Per Kuch Nehi LIkh Paya, Me Monday Se Pura Code Per Dhan dunga,
+        Please Sir Mera A Letter Accept Kariye.
+      </p>
+      <h3>Faithfuly </h3>
+      <h4>Sk Md Ashfdakur Rahman</h4>
+      <h6>04/14/2024</h6>
+    </div>
+  );
+};
